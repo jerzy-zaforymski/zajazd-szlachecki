46175041548fc68398082af5e1b3ffec0f2c0090
jerzy-zaforymski <86938586+jerzy-zaforymski@users.noreply.github.com>
2021-07-05 06:03:11 +0200

    Add files via upload

---
+++ stary_2013_1/rezerwacja.php
@@ -0,0 +1,156 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	<title>==========ZAJAZD SZLACHECKI - KRAKÓW========== Karczma Zajazd Szlachecki Zapraszamy do hotelu i restauracji. Oferujemy smaczne dania kuchni staropolskiej i komfortowe pokoje go¶cinne. Zapewniamy mi³± i profesjonaln± obs³ugê, oraz rodzinna atmosferê. Karczma Staropolska, Karczma Zajazd Szlachecki, Karczama Zajazd Szlachecki, Zajazd, Karczma, Staropolska karczma, wesele, wesela, komunie, imprezy komunijne, iprezy weselne, motel, hotel, biesiada, pokoje, noclegi, tanie pokoje, pokoje go¶cinne, zakwaterowanie, go¶ciniec, wypoczynek, odpoczynek, spanie, gospoda, tañce, hulanki, swawole, szlachta, szlachecki, dom wypoczynkowy, zjazdy, imprezy, bankiety, wesela, rauty, Kraków, go¶cie, pub, kawiarnia, klub, wczasy, apartament, ¿ywienie zbiorowe, wêdrowiec, dom wczasowy, pobyty weekendowe, imprezy towarzyskie, biesiady taneczne, restauracja, posi³ki, dania, jedzenie, jad³o, ¿ywienie, kuchnia, gotowanie, staropolska kuchnia, smaczna kuchnia, tradycyjna kuchnia, domowa kuchnia, potrawy, tradycyjne potrawy, przyjêcia, napoje, alkohol, drinki, kawa, dziczyzna, ¿ywno¶æ, piwo, cattering, bufet szwedzki, konferencje, sympozja, imprezy, organizacja</title>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-2">
+	<meta name="Description" content="Zapraszamy do hotelu i restauracji. Oferujemy smaczne dania kuchni staropolskiej i komfortowe pokoje go¶cinne. Zapewniamy mi³± i profesjonaln± obs³ugê, oraz rodzinn± atmosferê.  Karczma Staropolska, Karczma Zajazd Szlachecki, Zajazd, Karczma, Staropolska karczma, wesela, wesele, komunie, imprezy komunijne, iprezy weselne, motel, hotel, biesiada, pokoje, noclegi, tanie pokoje, pokoje go¶cinne, zakwaterowanie, go¶ciniec, wypoczynek, odpoczynek, spanie, gospoda, tañce, hulanki, swawole, szlachta, szlachecki, dom wypoczynkowy, zjazdy, imprezy, bankiety, wesela, rauty, Kraków, go¶cie, pub, kawiarnia, klub, wczasy, apartament, ¿ywienie zbiorowe, wêdrowiec, dom wczasowy, pobyty weekendowe, imprezy towarzyskie, biesiady taneczne, restauracja, posi³ki, dania, jedzenie, jad³o, ¿ywienie, kuchnia, gotowanie, staropolska kuchnia, smaczna kuchnia, tradycyjna kuchnia, domowa kuchnia, potrawy, tradycyjne potrawy, przyjêcia, napoje, alkohol, drinki, kawa, dziczyzna, ¿ywno¶æ, piwo, cattering, bufet szwedzki, konferencje, sympozja, imprezy, organizacja">
+	<meta name="Keywords" content="ZAJAZD SZLACHECKI - KRAKÓW,   Karczma Staropolska, Karczma Zajazd szlachecki, Karczama Zajazd Szlachecki, Zajazd, Karczma, Staropolska karczma, wesela, komunie, imprezy komunijne, iprezy weselne, motel, hotel, biesiada, pokoje, noclegi, tanie pokoje, pokoje go¶cinne, zakwaterowanie, go¶ciniec, wypoczynek, odpoczynek, spanie, gospoda, tañce, hulanki, swawole, szlachta, szlachecki, dom wypoczynkowy, zjazdy, imprezy, bankiety, wesele, wesela, rauty, Kraków, go¶cie, pub, kawiarnia, klub, wczasy, apartament, ¿ywienie zbiorowe, wêdrowiec, dom wczasowy, pobyty weekendowe, imprezy towarzyskie, biesiady taneczne, restauracja, posi³ki, dania, jedzenie, jad³o, ¿ywienie, kuchnia, gotowanie, staropolska kuchnia, smaczna kuchnia, tradycyjna kuchnia, domowa kuchnia, potrawy, tradycyjne potrawy, przyjêcia, napoje, alkohol, drinki, kawa, dziczyzna, ¿ywno¶æ, piwo, cattering, bufet szwedzki, konferencje, sympozja, imprezy, organizacja">
+	<link href="style.css" rel=stylesheet>
+	<script language="javascript" src="script.js" type="text/javascript"></script>
+</head>
+<body>
+<br />
+<table align="center" cellpadding="0" cellspacing="0" width="670" border="0">
+<tr><td><img src="graf/zajazd.jpg" alt="zajazd" /></td></tr>
+<tr>
+	<td>
+		<table align="center" cellpadding="0" cellspacing="0" width="670" border="0">
+		<tr>
+			<td width="50"><img src="graf/menu_4.gif" alt="menu_4" /></td>
+			<td><a href="index.html"><img src="graf/menu_str.gif" border="0" alt="strona g³ówna" /></a></td>
+			<td><a href="zajazd.html"><img src="graf/menu_zaj.gif" border="0" alt="zajazd i okolica" /></a></td>
+			<td><a href="hotel.html"><img src="graf/menu_hot.gif" border="0" alt="hotel" /></a></td>
+			<td><a href="restauracja.html"><img src="graf/menu_res.gif" border="0" alt="restauracja" /></a></td>
+			<td><a href="rezerwacja.php"><img src="graf/menu_rez.gif" border="0" alt="rezerwacja" /></a></td>
+			<td><a href="kontakt.html"><img src="graf/menu_kon.gif" border="0" alt="kontakt" /></a></td>
+			<td width="50"><img src="graf/menu_5.gif" alt="menu_4" /></td>
+		</tr>
+		</table>
+	</td>
+</tr>
+<tr><td><img src="graf/menu_3.jpg" alt="menu_3" /></td></tr>
+<tr>
+	<td style="background: url(graf/tlo.jpg)">
+		<table cellpadding="0" cellspacing="0" width="670" border="0">
+		<tr>
+			<td width="50" valign="top"><img src="graf/menu_1.gif" alt="menu_1" /></td>
+			<td width="570" align="center">
+
+<!-- modu³  -->
+				<div class="tytul">Rezerwacja</div><br />
+				
+				
+				<FORM action="rezerwacja.php" method="get">
+				<table cellpadding="0" cellspacing="15" border="0">
+				<tr>
+					<td width="55%" align="right">Imiê i nazwisko osoby rezerwuj±cej:</td>
+					<td width="45%"><input name="nazwisko" size="40" class="form"></td>
+				</tr>
+				<tr>
+					<td align="right">Telefon kontaktowy osoby rezerwuj±cej:</td>
+					<td><input name="telefon" size="15" class="form"></td>
+				</tr>
+				<tr>
+					<td align="right">Imiê i nazwisko go¶cia(i):</td>
+					<td><input name="gosc" size="40" class="form"></td>
+				</tr>
+				<tr>
+					<td valign="top" align="right">Okres pobytu:</td>
+					<td>
+						Od:&nbsp;&nbsp;<select class="form" name="oddzien">
+						<option selected value=01>01</option> <option value=02>02</option> <option value=03>03</option> 
+						<option value=04>04</option> <option value=05>05</option> <option value=06>06</option> 
+						<option value=07>07</option> <option value=08>08</option> <option value=09>09</option> 
+						<option value=10>10</option> <option value=11>11</option> <option value=12>12</option> 
+						<option value=13>13</option> <option value=14>14</option> <option value=15>15</option> 
+						<option value=16>16</option> <option value=17>17</option> <option value=18>18</option> 
+						<option value=19>19</option> <option value=20>20</option> <option value=21>21</option> 
+						<option value=22>22</option> <option value=23>23</option> <option value=24>24</option> 
+						<option value=25>25</option> <option value=26>26</option> <option value=27>27</option> 
+						<option value=28>28</option> <option value=29>29</option> <option value=30>30</option> 
+						<option value=31>31</option>
+						</select>
+						<select class="form" name="odmiesiac">
+						<option selected value=styczeñ>styczeñ</option> <option value=luty>luty</option> 
+						<option value=marzec>marzec</option> <option value=kwiecieñ>kwiecieñ</option> 
+						<option value=maj>maj</option> <option value=czerwiec>czerwiec</option> 
+						<option value=lipiec>lipiec</option> <option value=sierpieñ>sierpieñ</option> 
+						<option value=wrzesieñ>wrzesieñ</option> <option value=pa¼dziernik>pa¼dziernik</option> 
+						<option value=listopad>listopad</option> <option value=grudzieñ>grudzieñ</option></select> 
+						<select class="form" name="odrok"> 
+						<option value=2009>2011</option>
+						<option value=2010>2012</option>						</select>
+						<br />
+						Do:&nbsp;&nbsp;<select class="form" name="dodzien"> 
+						<option selected value=01>01</option> <option value=02>02</option> <option value=03>03</option> 
+						<option value=04>04</option> <option value=05>05</option> <option value=06>06</option> 
+						<option value=07>07</option> <option value=08>08</option> <option value=09>09</option> 
+						<option value=10>10</option> <option value=11>11</option> <option value=12>12</option> 
+						<option value=13>13</option> <option value=14>14</option> <option value=15>15</option> 
+						<option value=16>16</option> <option value=17>17</option> <option value=18>18</option> 
+						<option value=19>19</option> <option value=20>20</option> <option value=21>21</option> 
+						<option value=22>22</option> <option value=23>23</option> <option value=24>24</option> 
+						<option value=25>25</option> <option value=26>26</option> <option value=27>27</option> 
+						<option value=28>28</option> <option value=29>29</option> <option value=30>30</option> 
+						<option value=31>31</option>
+						</select>
+						<select class="form" name="domiesiac">
+						<option selected value=styczeñ>styczeñ</option> <option value=luty>luty</option> 
+						<option value=marzec>marzec</option> <option value=kwiecieñ>kwiecieñ</option> 
+						<option value=maj>maj</option> <option value=czerwiec>czerwiec</option> 
+						<option value=lipiec>lipiec</option> <option value=sierpieñ>sierpieñ</option> 
+						<option value=wrzesieñ>wrzesieñ</option> <option value=pa¼dziernik>pa¼dziernik</option> 
+						<option value=listopad>listopad</option> <option value=grudzieñ>grudzieñ</option></select> 
+						<select class="form" name="dorok"> 
+						<option value=2009>2011</option>
+						<option value=2010>2012</option>					</td>
+				</tr>
+				<tr>
+					<td valign="top" align="right">Forma p³atno¶ci:</td>
+					<td>
+						<input name="forma" type="radio" value="gotowka" checked>gotówka<br />
+						<input name="forma" type="radio" value="karta kredytowa">karta kredytowa
+					</td>
+				</tr>
+				<tr>
+					<td valign="top" align="right">Rodzaj pokoju oraz ilo¶æ rezerwowanych pokoi:</td>
+					<td>
+						pokój 1-osobowy, ilo¶æ&nbsp;&nbsp;<input class="form" maxLength="2" name="pokoj1" size="1"><br />
+			            pokój 2-osobowy, ilo¶æ&nbsp;&nbsp;<input class="form" maxLength="2" name="pokoj2" size="1"><br />
+						pokój 3-osobowy, ilo¶æ&nbsp;&nbsp;<input class="form" maxLength="2" name="pokoj3" size="1"><br />
+						pokój 4-osobowy, ilo¶æ&nbsp;&nbsp;<input class="form" maxLength="2" name="pokoj4" size="1">
+					</td>
+				</tr>
+				<tr>
+					<td valign="top" align="right">Prosimy wype³niæ jedno z trzech pól - po otrzymaniu formularza skontaktujemy siê w wybrany przez Pañstwa sposób (email, telefon lub fax) celem potwierdzenia rezerwacji:</td>
+					<td>
+						<input name="potwierdzenie" type="radio" checked value="e-mail:">e-mailem&nbsp;&nbsp;<input class="form" name="email" size="14"><br />
+						<input name="potwierdzenie" type="radio" value="telefon:">telefonicznie, tel.&nbsp;&nbsp;<input class="form" name="tel" size="14"><br />
+						<input name="potwierdzenie" type="radio" value="fax:">faxem, fax&nbsp;&nbsp;<input class="form" name="fax" size="14">
+					</td>
+				</tr>
+				<tr>
+					<td valign="top" align="right">Uwagi:</td>
+					<td><TEXTAREA class="form" cols=30 name="uwagi" rows=10></TEXTAREA></td>
+				</tr>
+				<tr>
+					<td colspan="2" align="center"><br /><input class="form_send" name=Submit type=submit value="Wy¶lij"></td>
+				</tr>
+				</table>
+				
+				</form>
+				
+<!-- koniec: modu³ -->
+
+			</td>
+			<td width="50" valign="top"><img src="graf/menu_2.gif" alt="menu_2" /></td>
+		</tr>
+		</table>
+	</td>
+</tr>
+<tr><td><img src="graf/stopka.jpg" alt="stopka" /></td></tr>
+</table>
+
+</body>
+</html>
